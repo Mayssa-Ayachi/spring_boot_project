95e7a1097d4fd0d11204545766392c662236be83
Mayssa Ayachi <ayachi.mayssa@ensi-uma.tn>
2023-04-19 11:44:16 +0100

    test path

--- backend/src/main/java/com/backend/configs/SecurityConfig.java
+++ backend/src/main/java/com/backend/configs/SecurityConfig.java
@@ -30,7 +30,6 @@ public class SecurityConfig {
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(authenticationJwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests()
-                .requestMatchers("/api/auth/**").permitAll()
                 .requestMatchers("/api/all").permitAll()
                 .requestMatchers("/api/student").hasRole("STUDENT")
                 .requestMatchers("/api/teacher").permitAll()
